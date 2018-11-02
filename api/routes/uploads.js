88ac3bac54e6f03f38ece52b35cf5973d5861179
Karol Zientek <karol.zientek001@protonmail.com>
2018-11-02 23:19:31 +0100

    + edited request parameters from ":filePath" to ":dllId"

--- api/routes/uploads.js
+++ api/routes/uploads.js
@@ -3,6 +3,6 @@ const router = express.Router();
 const uploadsController = require('../controllers/uploads');
 const checkAuth = require('../middleware/checkAuth');
 
-router.get('/:filePath', checkAuth, uploadsController);
+router.get('/:dllId', checkAuth, uploadsController);
 
 module.exports = router;
